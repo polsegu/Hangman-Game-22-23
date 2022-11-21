86ae5dcb2804d4fc76ea83a2607ae60dfc870ba2
NahuelAparicio <nahuel.aparicio@enti.cat>
2022-11-22 00:22:58 +0100

    LogIn & Register

---
+++ app/src/main/java/com/example/hanggame/LoginActivity.kt
@@ -0,0 +1,71 @@
+package com.example.hanggame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hanggame.databinding.ActivityLoginBinding
+import android.view.View
+import android.widget.Toast
+import android.util.Patterns
+
+import com.google.firebase.auth.FederatedAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthProvider
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginButton.setOnClickListener{
+            val username = binding.userInput.text.toString()
+            val password = binding.passInput.text.toString()
+
+            firebaseAuth.signInWithEmailAndPassword(username, password)
+                .addOnSuccessListener {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+                    finish()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Incorrect username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.registerButton.setOnClickListener {
+            val email = binding.userInput.text.toString()
+            val password = binding.passInput.text.toString()
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful)
+                    {
+                        Toast.makeText(this, "Registrared succesfully", Toast.LENGTH_SHORT).show()
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "Invalid registration", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        binding.userInput.setOnFocusChangeListener{ view, hasFocus ->
+            if(!hasFocus)
+            {
+                val username = binding.userInput.text.toString()
+                if(!Patterns.EMAIL_ADDRESS.matcher(username).matches())
+                    binding.userInput.error = "Invalid email"
+                else
+                    binding.userInput.error = null
+            }
+        }
+
+    }
+}
