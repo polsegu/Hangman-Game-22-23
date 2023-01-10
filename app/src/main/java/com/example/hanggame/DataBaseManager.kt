8398e337167b775ba1b49287ef5bd1939a589f3a
NahuelAparicio <nahuel.aparicio@enti.cat>
2023-01-10 05:24:51 +0100

    Starting RealTime DDBB

---
+++ app/src/main/java/com/example/hanggame/DataBaseManager.kt
@@ -0,0 +1,14 @@
+package com.example.hanggame
+
+import com.google.firebase.database.FirebaseDatabase
+
+class DataBaseManager {
+
+    private lateinit var database: FirebaseDatabase
+
+    fun saveData(name: String?, score : Int?)
+    {
+       // database = FirebaseDatabase.getInstance().getReference("rank")
+
+    }
+}
---
+++ app/src/main/java/com/example/hanggame/User.kt
@@ -0,0 +1,3 @@
+package com.example.hanggame
+
+data class User(val name: String? = null, val score: Int? = 0)
